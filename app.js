f91ab92071f4e677a650fc01436675d5805ab7f9
RACE - Rasmus Cederdorff <rasmus@cederdorff.com>
2025-12-05 09:04:17 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+"use strict"; // Enable strict mode for better error checking
+
+// #0: Listen for page load
+window.addEventListener("load", initApp); // When the page is loaded, run initApp function
+
+// #1: Initialize the app
+function initApp() {
+  console.log("initApp: app.js is running 🎉"); // Log to the console that the app is running
+}
